0bf3a7792aaa98e714d49a3cbe026ccd709697f2
Ming Han <minghan.xu@gatech.edu>
2019-04-24 02:10:53 -0400

    DCP_2_3

--- DCP_2_3.py
+++ DCP_2_3.py
@@ -37,3 +37,35 @@ def print_zigzag(sentence, k):
     return
 
 
+# determine space between two letters in the same row, constant time
+def get_spaces(row, desc, k):
+    max_spaces = (k-1) * 2 -1
+    if desc:
+        spaces = max_spaces - row * 2
+    else:
+        spaces = max_spaces - (k - 1 -row) * 2
+    return spaces
+
+# decide whether the part is ascending or descending, constant time
+def is_descending(index, k):
+    return index % (2 * (k-1)) < k-1
+
+# O(kn) time, where n is the length of sentence string
+# calculate the space between two consecutive letters in each line
+# space is the offset index in the original sentence string
+def print_zigzag_2(sentence, k):
+    n = len(sentence)
+
+    for row in range(k):
+        i = row
+        line = [" " for _ in range(n)]
+
+        while i < n:
+            line[i] = sentence[i]
+            desc = is_descending(i,k)
+            spaces = get_spaces(row, desc, k)
+            i += spaces + 1
+    
+        print("".join(line))
+
+
