902ce36c22b7459b96807a65c0a6d544465404de
Ming Han <minghan.xu@gatech.edu>
2019-05-01 08:49:37 -0400

    DCP_3_3

---
+++ DCP_3_3.py
@@ -0,0 +1,4 @@
+# Given a linked list, rearrange the node values such that they appear in alteranting low --> high --> low --> high --> form
+
+# Example, Input: 1 --> 2 --> 3 --> 4 --> 5
+# Output: 1 --> 3 --> 2 --> 5 --> 4
